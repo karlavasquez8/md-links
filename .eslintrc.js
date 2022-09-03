86ca963ca0e396f5568219c6b51eb848539fa268
karla vasquez <karlavasquez817@gmail.com>
2022-09-03 18:39:01 -0500

    feat:search files and directory

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "prefer-destructuring": 0,
+        "import/extensions": 0,
+        "import/prefer-default-export": 0
+    }
+}
--- index.js
+++ index.js
@@ -1,3 +1,69 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt()
+
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const nodePath = require('node:path')
+
+function isFile(path) {
+  return fs.statSync(path).isFile()
+}
+
+function openFile(path) {
+  return fs.readFileSync(path, 'utf8')
+}
+//me devuelve el contenido'
+function isDirectory(path) {
+  return fs.statSync(path).isDirectory()
+}
+
+function openDir(path) {
+  return fs.readdirSync(path)
+}
+
+function scanlinks(path) {
+  const contentFile = openFile(path)
+  //convirtiendo a string de html el archivo md
+  const stringHtml = md.render(contentFile)
+  //con el jsdom se convierte a html 
+  const htmlDom = new JSDOM(stringHtml)
+  const nodeList = htmlDom.window.document.querySelectorAll('a')
+
+  const listLink = [];
+
+  nodeList.forEach((link) => {
+    listLink.push({ href: link.href, text: link.innerHTML, file: path })
+  })
+  return listLink
+}
+
+
+const mdLinks = (path, config = { validate: false }) => {
+  if (isFile(path)) {
+    console.log(scanlinks(path))
+  }
+  if (isDirectory(path)) {
+    const files = openDir(path)
+    console.log(files)
+    let listLinks = []
+
+    files.forEach((file) => {
+      //ruta completa del directorio
+      const fullPath = nodePath.join(path, file)
+      listLinks = [...listLinks, ...scanlinks(fullPath)]
+    })
+    console.log(listLinks)
+  }
+
+
+
+  // arraylist =openDir(path)
+  // arraylist.forEach()
 };
+
+module.exports = mdLinks;
+
+mdLinks('storage', { validate: true });
