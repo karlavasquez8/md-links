ca951b4a7707f774c228c547fa2fef191acbbfc2
karla vasquez <karlavasquez817@gmail.com>
2022-09-08 11:21:49 -0500

    feat:agregando la funcion para hacer peticion al servidor

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const fs = require('fs');
 
 const MarkdownIt = require('markdown-it');
-const md = new MarkdownIt()
+const md = new MarkdownIt()//constructor del md
 
 const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const { JSDOM } = jsdom;//constructor de jsdom
 
 const nodePath = require('node:path')
 
+const axios = require('axios');
 
 
+//funciones
+//funcion para saber si es un archivo
 function isFile(path) {
   return fs.statSync(path).isFile()
 }
 
+//funcion para abrir un archivo
 function openFile(path) {
   return fs.readFileSync(path, 'utf8')
 }
-//me devuelve el contenido'
+//funcion para saber si es un directorio'
 function isDirectory(path) {
   return fs.statSync(path).isDirectory()
 }
 
+//funcion para abrir un directorio
 function openDir(path) {
   return fs.readdirSync(path)
 }
 
+//convirtiendo a ruta absoluta
+function pathAbsolute(route) {
+  return path.isAbsolute(route) ? route : path.resolve(route)
+}
+
+//funcion para verificar si existe una ruta
+function pathExist(path) {
+  return fs.existsSync(path)
+}
 
 function scanLinks(path) {
   const contentFile = openFile(path)
@@ -41,7 +55,44 @@ function scanLinks(path) {
     listLink.push({ href: link.href, text: link.innerHTML, file: path })
   })
   return listLink
-}
+};
+
+//HTTP
+//Funcion para validar los links que se extrajeron
+const validateServer = (path) => {
+  return axios(path)
+    .then((data) => {
+      if (data.status === 200) {
+        return {
+          path,
+          status: data.status,
+          menssage: 'ok'
+        };
+      }
+    })
+    .catch((error) => {
+      return ({
+        path,
+        status: error.response.status,
+        menssage: 'fail'
+      })
+    })
+};
+
+validateServer('https://www.geeksforgeeks.org/node-js-fs-readfilesync-method/').then((data) => {
+  console.log(data)
+})
+
+//new Promise((resolve, reject) => {
+//   axios.get(paths)
+//     .then((response) => {
+//       console.log(response);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// })
+
 
 
 const mdLinks = (path, config = { validate: false }) => {
@@ -54,10 +105,10 @@ const mdLinks = (path, config = { validate: false }) => {
 
     files.forEach((file) => {
       //ruta completa del directorio
-      const fullPath = nodePath.join(path, file)
+      // const fullPath = nodePath.join(path, file)
+      const fullPath = nodePath.resolve(nodePath.join(path, file))
       listLinks = [...listLinks, ...scanLinks(fullPath)]
     })
-    console.log(listLinks)
   }
 
 
@@ -74,4 +125,4 @@ module.exports = {
   scanLinks,
 }
 
-mdLinks('storage', { validate: true });
+mdLinks('storage/file.md', { validate: true });
